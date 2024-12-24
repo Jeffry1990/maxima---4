4e99997e2f7f8f05a183adb6526c59bdbbbe80d2
Jeffry1990 <warlock_artur@mail.ru>
2024-12-24 22:54:54 +0300

    Добавьте файлы проекта.

---
+++ maxima - 4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace maxima___4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+            public class School
+        {
+            public string name;
+            public uint count;
+
+            public School(string name, uint count)
+            {
+                this.name = name;
+                this.count = count;
+            }
+
+            public string getName() { return name; }
+            public uint getCount() { return count; }
+            public removeStudent() { count--; }
+
+        }
+    }
+}
+
